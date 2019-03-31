51963e82105e5c8d1d85b5cf0d5ca91e12aedf52
Aash <ashankar@heartpoints.org>
2019-03-31 10:24:16 -0700

    Adding Organization to side bar #115

--- src/components/castleRisk/CastleRisk.tsx
+++ src/components/castleRisk/CastleRisk.tsx
@@ -13,7 +13,7 @@ export const CastleRisk = (props) => {
     return  <div className={classes.content}>
         <h1>Castle Risk</h1>
         <hr></hr>
-        <p>Phase {phaseName}</p>
+        <p>Phase {phaseName.value}</p>
         {phase == Phase.Welcome && <button onClick={() => updateState({phase: Phase.AddPlayer})}>Begin Game</button>}
         {phase == Phase.AddPlayer && <div>
             <input autoFocus type="text" value={player} onChange={e => updateState({player: e.target.value})} />
